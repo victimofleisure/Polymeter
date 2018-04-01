8cbf0e289cd4d852b4b81a35ed3f13822e2b18e8
Chris Korda <ckorda@gmail.com>
2018-04-01 15:41:35 -0400

    add icon, fix links

--- docs/genGitHub.bat
+++ docs/genGitHub.bat
@@ -40,6 +40,18 @@ rem cd ..
 ren issues.html issues.htm
 echo y | C:\Chris\tools\fsr issues.htm "<div id=body>" "<div id=widebody>"
 ren issues.htm issues.html
+ren links.html link.htm
+echo y | C:\Chris\tools\fsr links.htm "\"http://chordease.sourceforge.net/\"" "\"https://victimofleisure.github.io/ChordEase/\""
+echo y | C:\Chris\tools\fsr links.htm "\"http://ffrend.sourceforge.net/\"" "\"https://victimofleisure.github.io/FFRend/\""
+echo y | C:\Chris\tools\fsr links.htm "\"http://fractice.sourceforge.net/\"" "\"https://victimofleisure.github.io/Fractice/\""
+echo y | C:\Chris\tools\fsr links.htm "\"http://mixere.sourceforge.net/\"" "\"https://victimofleisure.github.io/Mixere/\""
+echo y | C:\Chris\tools\fsr links.htm "\"http://polymeter.sourceforge.net/\"" "\"https://victimofleisure.github.io/Polymeter/\""
+echo y | C:\Chris\tools\fsr links.htm "\"http://potterdraw.sourceforge.net/\"" "\"https://victimofleisure.github.io/PotterDraw/\""
+echo y | C:\Chris\tools\fsr links.htm "\"http://sourceforge.net/projects/triplight/\"" "\"https://github.com/victimofleisure/TripLight/\""
+echo y | C:\Chris\tools\fsr links.htm "\"http://waveshop.sourceforge.net/\"" "\"https://victimofleisure.github.io/WaveShop/\""
+echo y | C:\Chris\tools\fsr links.htm "\"http://whorld.sourceforge.net/\"" "\"https://victimofleisure.github.io/Whorld/\""
+echo y | C:\Chris\tools\fsr links.htm "\"http://whorld.org/\"" "\"https://victimofleisure.github.io/Whorld/\""ren link.htm links.html
+ren links.htm link.html
 goto exit
 :err
 pause Error!
