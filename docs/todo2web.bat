66b5fd75f2a975b95b7613c501488f2be3cf379f
Chris Korda <victimofleisure@gmail.com>
2018-04-13 13:17:53 -0400

    update issues list

--- docs/todo2web.bat
+++ docs/todo2web.bat
@@ -1,6 +1,6 @@
 C:\Chris\MyProjects\tbl2web\release\tbl2web "..\Polymeter ToDo.txt" issues.html issues.txt "Polymeter Issues"
-navgen template.html . issues.html
+C:\Chris\tools\navgen template.html . issues.html
 ren issues.html issues.htm
-echo y | fsr issues.htm "<div id=body>" "<div id=widebody>"
+echo y | C:\Chris\tools\fsr issues.htm "<div id=body>" "<div id=widebody>"
 ren issues.htm issues.html
 
