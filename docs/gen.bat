ff85c3fae1f4947a7fbcf6e0f381a93cfdbf47ec
Chris Korda <victimofleisure@users.noreply.github.com>
2024-10-28 11:57:47 +0100

    update language in html tags

--- docs/gen.bat
+++ docs/gen.bat
@@ -3,9 +3,13 @@ C:\Chris\MyProjects\tbl2web\release\tbl2web "..\Polymeter ToDo.txt" issues.html 
 if errorlevel 1 goto err
 attrib /s tools\*.* +r
 if errorlevel 1 goto err
+attrib /s Help\*.* +r
+if errorlevel 1 goto err
 C:\Chris\tools\navgen template.html .
 if errorlevel 1 goto err
 attrib /s tools\*.* -r
+if errorlevel 1 goto err
+attrib /s Help\*.* -r
 if errorlevel 1 goto err
 C:\Chris\MyProjects\FixSelfUrl\Release\FixSelfUrl *.html
 if errorlevel 1 goto err
